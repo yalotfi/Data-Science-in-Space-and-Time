21e6097a30989154c528f106f1a408cdda6f5a39
Yaseen Lotfi <yalotfi@outlook.com>
2017-04-11 21:54:31 -0400

    Subsetting and indexing real data

--- Lab-01-Introduction/Lab-01-Skeleton.R
+++ Lab-01-Introduction/Lab-01-Skeleton.R
@@ -4,7 +4,7 @@
 # Date: 4/10/2017             #
 # Title: Lab 1 Skeleton Files #
 # Description:                #
-#   Dealing with Data         #
+#   Working with real data    #
 ###############################
 ###############################
 
@@ -12,10 +12,10 @@
 # First, a statement to clean your R environment
 # Second, a statement which sets your working directory
 # Third, check that your working directory was set to the proper destination
+
 rm(list = ls())
 setwd("C:/Users/yalot/OneDrive/CompSci/GitHub/Data-Science-in-Space-and-Time/Lab-01-Introduction")
 getwd()
-
 
 ##############
 ## Packages ##
@@ -59,10 +59,8 @@ library(raster)
 # Tell R how to read this data, splitting each data point everytimme it finds a comma
 #   *.csv == "comma-separated-value"
 
-
 inst_raw_data <- read.csv2("data/hd2014.csv", sep = ",")
 admin_raw_data <- read.csv("data/adm2014.csv", sep = ",")
-
 
 # What command and arguments would you use if the data was tab-delimited?
 
@@ -89,6 +87,72 @@ rm(inst_raw_data, admin_raw_data, raw_data_merge)
 
 # Note, if you try to remove an object that does not exist, R will ignore it and only output a warning.
 
+#############################
+### Subsetting & Indexing ###
+#############################
+
+# Now that we aggragated our raw_data, we can extract the features that we are interested
+# in using for our analysis. For the purposes of this exercise, we will not get into what
+# each column means. Instead focus on the mechanics of how we extract the information we want.
+# In the future, you will want to think about how to slice and dice your data.
+
+# STEP 2: SUBSETTING
+# The next two lines are straightforward. We want to pull a subset of the raw data only where
+# the feature ICLEVEL is equal to 9. This command will reduce the number of rows in our data.
+
+institutions <- subset(raw_data, HLOFFER == 9)
+
+# STEP 1: INDEXING
+# Indexing is extremely useful. While we take a manual approach here, in the future you will
+# use conditional logic to subset and index. For now, all we are doing is pulling which columns
+# we are interested in. In this instance, we are reducing the number of features in our dataset
+# and saving it in a new data frame called institution. 
+
+# Note the syntax of an index:
+
+# objName[rowNumber, colNumber]  # This will return a single value at this position (think cell addresses in excel)
+
+# This syntax is very useful. We could assign a new value at that index position:
+
+# objName[rowNumber, colNumber] <- someValue
+
+# If we are only interested in rows or columns, we can still use indexing as follow:
+# This will return a row-wise or column-wise vector, or 1D list of values. This can be very useful as well,
+# but you must recognize what you are indexing.
+
+# objName[, colNumber]  OR  objName[rowNumber, ]
+
+# You will probably not know the row or column numbers more oftent than not. In this case, we can use
+# conditional logic with the following syntax:
+
+# objName[conditionalTestRow, conditionalTestCol]  # You can include one or both tests.
+
+# There are a lot of way to construct a conditional test, so it is worth looking through some
+# of the resources in the main GitHub repository. We will also emphasize when we use it
+# in the future so you can learn to recognize this tool and build an intuition for it.
+
+# Finally, we happen to no which columns we want to keep, so we will index by column
+# and save the new dataframe in a variable called 'institutions'. Notice we are overwriting
+# the object we already created. This is okay as long as you are aware of what you are doing.
+# A lot of errors in the future might come frome the coder unintentionally overwriting a data
+# object without realizing it. In this case, we are sure we want to do this.
+
+institutions <- institutions[,c(1:2,4:5,66:67, 80, 86, 92, seq(118, 144, 2))]
+
+# In this use of indexing, we are listing column numbers that we want to extract.
+# The colon operator simply says, "1 to 2" or 66:67. The seq() command will start
+# at the first argument, 118 and count up to the second argument, 144, incrementing by 2.
+# To understand the code, copy seq(118, 144, 2) into your console to see exactly what it is doing.
+
+# Now we have reduced the dimensionality of our data to something relevant to our analysis. We can
+# create new columns by passing some computation over other columns. In this case, we will divide the
+# number of students admitted by the number of applicants and save the admission rate to a new columns
+# we define as 'percent_admit'.
+
+institutions$percent_admit <- institutions$ADMSSN/institutions$APPLCN
+
+# The $ operator is very important here.
+
 ########################
 ### Explore the Data ###
 ########################
@@ -105,9 +169,11 @@ rm(inst_raw_data, admin_raw_data, raw_data_merge)
 # One way we would use names(x) is for renaming columns. For example,
 # we can rename the first column to "ID" with following code:
 
+names(raw_data)[1]
 names(raw_data)[1] <- "ID"  # Tells R to assign the string value, "ID" to the first element of names(raw_data)
+names(raw_data)[1]
 
-# Note what the old name was and whether or not is changes.
+# Note what the old name was and whether or not it changed.
 
 # 2) Show the number of rows and columns, run:
 #   nrow(objName)
@@ -121,6 +187,23 @@ names(raw_data)[1] <- "ID"  # Tells R to assign the string value, "ID" to the fi
 # on the data object in the RStudio IDE. Get in the habit of using the console because
 # you will become a more efficient and productive programmer.
 
+# 4) View Data Structure because you should always be thinking of the data type
+#   str(objName)
+
+# 5) Summary Statistics of each variable in your dataser:
+#   summary(objName)
+
+# 5) Is there missing Data? We can count the number of missing data by column.
+
+apply(raw_data, 2, function(x) sum(is.na(x)))
+
+# There is a lot happening in this line, so let's break it down. The apply() command
+# will apply a function over a given data set, defined in the first argument. The 2 tell
+# apply() that it should apply the function by column. The third argument is an anonymous
+# function because we only use it this one time. We are going to ask if 'x' NA or not. We then
+# sum all the TRUE cases, telling us how many null values exist in each column of our data frame.
+# This is extremely useful when getting a sense of how 'dirty' your data is. Even if you don't
+# get what the code does right away, I recommend using this code in future debugging exercises.
 
 
 
